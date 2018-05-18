23c1f7c5ea4d95f24a11f170f740012e538b660a
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2018-05-18 14:53:58 -0600

    STYLE: split multilabel mask into 4d (or 5d) mask

--- simplereg/application/split_multilabel_mask.py
+++ simplereg/application/split_multilabel_mask.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import numpy as np
 import SimpleITK as sitk
+import nibabel as nib
 
 import pysitk.python_helper as ph
 import pysitk.simple_itk_helper as sitkh
@@ -12,8 +13,8 @@ import simplereg.utilities as utils
 
 
 ##
-# split multi-label mask into 4d image where each timepoint corresponds to
-# independent mask
+# split multi-label mask into 4d (or 5d) image where each timepoint corresponds
+# to independent mask
 # \date       2018-04-25 11:50:08-0600
 #
 # \return     exit code
@@ -21,8 +22,8 @@ import simplereg.utilities as utils
 def main():
 
     parser = argparse.ArgumentParser(
-        description="Split multi-label mask into 4D image where each "
-        "timepoint corresponds to independent mask.",
+        description="Split multi-label mask into 4D (or 5D) image where each "
+        "timepoint corresponds to an independent mask label.",
         prog=None,
         epilog="Author: Michael Ebner (michael.ebner.14@ucl.ac.uk)",
     )
@@ -39,6 +40,13 @@ def main():
         required=1,
     )
     parser.add_argument(
+        "--use-5d",
+        help="Set output to be 4D or 5D",
+        type=int,
+        required=0,
+        default=0,
+    )
+    parser.add_argument(
         "-v", "--verbose",
         help="Turn on/off verbose output",
         type=int,
@@ -47,9 +55,14 @@ def main():
     )
     args = parser.parse_args()
 
-    labels_sitk = sitk.ReadImage(args.filename)
+    if args.use_5d:
+        labels_sitk = sitk.ReadImage(args.filename)
+        nda = sitk.GetArrayFromImage(labels_sitk).astype(np.uint8)
 
-    nda = sitk.GetArrayFromImage(labels_sitk).astype(np.uint8int8)
+    else:
+        labels_nib = nib.load(args.filename)
+        nda = labels_nib.get_data().astype(np.uint8)
+
     n_labels = int(np.max(nda))
 
     # split labels into separate components
@@ -60,13 +73,20 @@ def main():
         indices += (label * np.ones(len(indices[0]), dtype=np.uint8),)
         nda_4d[indices] = 1
 
-    # Create sitk.Image object
-    labels_4d_sitk = sitk.GetImageFromArray(nda_4d)
-    labels_4d_sitk.SetOrigin(labels_sitk.GetOrigin())
-    labels_4d_sitk.SetSpacing(labels_sitk.GetSpacing())
-    labels_4d_sitk.SetDirection(labels_sitk.GetDirection())
+    if args.use_5d:
+        labels_4d_sitk = sitk.GetImageFromArray(nda_4d)
+        labels_4d_sitk.SetOrigin(labels_sitk.GetOrigin())
+        labels_4d_sitk.SetSpacing(labels_sitk.GetSpacing())
+        labels_4d_sitk.SetDirection(labels_sitk.GetDirection())
 
-    sitkh.write_nifti_image_sitk(labels_4d_sitk, args.output)
+        sitkh.write_nifti_image_sitk(labels_4d_sitk, args.output)
+
+    else:
+        labels_4d_nib = nib.Nifti1Image(
+            nda_4d, affine=labels_nib.affine, header=labels_nib.header)
+        labels_4d_nib.set_data_dtype(np.uint8)
+        ph.create_directory(os.path.dirname(args.output))
+        nib.save(labels_4d_nib, args.output)
 
     if args.verbose:
         ph.show_niftis([args.filename, args.output])
